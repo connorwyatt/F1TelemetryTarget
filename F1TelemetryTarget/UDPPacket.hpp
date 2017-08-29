29cd8fb58971dd891b6992dfecbfcd496d463e00
Connor Wyatt <connorwyatt1@gmail.com>
2017-08-29 17:36:26 +0100

    add data struct

---
+++ F1TelemetryTarget/UDPPacket.hpp
@@ -0,0 +1,16 @@
+//
+//  UDPPacket.hpp
+//  F1TelemetryTarget
+//
+//  Created by Connor Wyatt on 29/08/2017.
+//  Copyright © 2017 Connor Wyatt. All rights reserved.
+//
+
+#ifndef UDPPacket_hpp
+#define UDPPacket_hpp
+
+#include <stdio.h>
+
+struct UDPPacket;
+
+#endif /* UDPPacket_hpp */
